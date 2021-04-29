5da5219a37f347b0902dc13f7750ec85e0ad33dd
Lenar Badykov <lenarbadykov@gmail.com>
2021-04-30 00:34:16 +0300

    Initial project

---
+++ RickAndMorty/RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Ленар Бадыков on 30.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
